d8810df4298e71845b36578291c5d93748e63c29
Ayoub A <m.ayoub@live.nl>
2021-09-17 10:15:50 +0200

    Update S3_Multiple_Tables_student.sql

--- src/opdracht/S3_Multiple_Tables_student.sql
+++ src/opdracht/S3_Multiple_Tables_student.sql
@@ -30,18 +30,18 @@
 -- Produceer een overzicht van alle cursusuitvoeringen; geef de
 -- code, de begindatum, de lengte en de naam van de docent.
 DROP VIEW IF EXISTS s3_1; CREATE OR REPLACE VIEW s3_1 AS                                                     -- [TEST]
-SELECT c.code, u.begindatum, c.lengte, m.naam FROM uitvoeringen u, medewerkers m, cursussen c WHERE c.code = u.cursus AND u.docent = m.mnr
+SELECT c.code, u.begindatum, c.lengte, m.naam FROM uitvoeringen u, medewerkers m, cursussen c WHERE c.code = u.cursus AND u.docent = m.mnr;
 
 
 -- S3.2.
 -- Geef in twee kolommen naast elkaar de achternaam van elke cursist (`cursist`)
 -- van alle S02-cursussen, met de achternaam van zijn cursusdocent (`docent`).
 DROP VIEW IF EXISTS s3_2; CREATE OR REPLACE VIEW s3_2 AS                                                     -- [TEST]
-SELECT crst.naam cursist, dcnt.naam docent
-FROM medewerkers crst
-         INNER JOIN inschrijvingen i ON crst.mnr = i.cursist and i.cursus = 'S02'
+SELECT c.naam cursist, d.naam docent
+FROM medewerkers c
+         INNER JOIN inschrijvingen i ON c.mnr = i.cursist and i.cursus = 'S02'
          INNER JOIN uitvoeringen u ON i.begindatum = u.begindatum and u.cursus = i.cursus
-         INNER JOIN medewerkers dcnt ON dcnt.mnr = u.docent;
+         INNER JOIN medewerkers d ON d.mnr = u.docent;
 
 -- S3.3.
 -- Geef elke afdeling (`afdeling`) met de naam van het hoofd van die
@@ -58,12 +58,12 @@ SELECT m.naam as afdeling, a.naam as hoofd, a.locatie FROM afdelingen a, medewer
 -- S3.5.
 -- Geef de namen van alle cursisten die staan ingeschreven voor de cursus S02 van 12 april 2019
 DROP VIEW IF EXISTS s3_5; CREATE OR REPLACE VIEW s3_5 AS                                                     -- [TEST]
-SELECT m.naam FROM uitvoeringen u, inschrijvingen i, medewerkers m WHERE i.cursus = u.cursus AND i.cursus = 'S02' AND i.begindatum = '2019-04-12' AND i.cursist = m.mnr
+SELECT m.naam FROM uitvoeringen u, inschrijvingen i, medewerkers m WHERE i.cursus = u.cursus AND i.cursus = 'S02' AND i.begindatum = '2019-04-12' AND i.cursist = m.mnr;
 
 -- S3.6.
 -- Geef de namen van alle medewerkers en hun toelage.
 DROP VIEW IF EXISTS s3_6; CREATE OR REPLACE VIEW s3_6 AS
-SELECT m.naam, s.toelage FROM medewerkers m, schalen s WHERE (m.maandsal >= s.ondergrens AND m.maandsal <= s.bovengrens )
+SELECT m.naam, s.toelage FROM medewerkers m, schalen s WHERE (m.maandsal >= s.ondergrens AND m.maandsal <= s.bovengrens );
 
 
 
